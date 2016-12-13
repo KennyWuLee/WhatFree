e1299f296cdf94dd350f902ec1e7ca81a3db1307
KennyWuLee <krischer.till@gmail.com>
2016-12-13 01:38:23 -0500

    change to pth

--- whatfree.sh
+++ whatfree.sh
@@ -3,17 +3,17 @@
 username=""
 password=""
 downloadDir="/path/to/downloadfolder/"
-
+url="passtheheadphones.me"
 
 free=$(curl -L -s -b "cookie.txt" -c "cookie.txt"\
- "https://what.cd/torrents.php?freetorrent=1")
-if echo "$free" | grep "<title>Login :: What.CD</title>" > /dev/null
+ "https://$url/torrents.php?freetorrent=1")
+if echo "$free" | grep "<title>Login ::" > /dev/null
 then
 	curl -L -s -b "cookie.txt" -c "cookie.txt"\
 	 -d "username=$username&password=$password&keeplogged=1" \
-	 "https://what.cd/login.php" > /dev/null
+	 "https://$url/login.php" > /dev/null
 	free=$(curl -L -s -b "cookie.txt" -c "cookie.txt"\
-	 "https://what.cd/torrents.php?freetorrent=1")
+	 "https://$url/torrents.php?freetorrent=1")
 fi
 echo "$free" |\
  sed -n '/action=download/ s/.*href..\(.*\)..class.*/\1/p' |\
@@ -21,5 +21,5 @@ echo "$free" |\
  while read link
  do
  	wget -O "$downloadDir$(echo $link | sed 's/.*download.id=\([0-9]*\).*/\1/').torrent"\
- 	 -nc "https://what.cd/$link"
+ 	 -nc "https://$url/$link"
  done 
